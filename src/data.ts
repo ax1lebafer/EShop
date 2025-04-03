3f72ce3b91ff5324ed42dd1d41ad625e0fc27688
ax1lebafer <raul8917@gmail.com>
2025-04-03 17:48:23 +0400

    Add new product

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,5 @@
 import byzS8521 from './assets/byz-s8521.png';
+import earpods1 from './assets/earpods_1.png';
 import earpods2 from './assets/earpods_2.png';
 import { IProduct } from './types.ts';
 export const HEADPHONES: IProduct[] = [
@@ -6,29 +7,43 @@ export const HEADPHONES: IProduct[] = [
     id: 1,
     img: byzS8521,
     title: 'Apple BYZ S852I',
-    price: 1000,
-    rate: 4.3,
+    price: 2927,
+    rate: 4.7,
+    old_price: 3527,
   },
   {
     id: 2,
-    img: byzS8521,
-    title: 'Apple BYZ S852I',
-    price: 1000,
-    rate: 4.3,
-    old_price: 600,
+    img: earpods1,
+    title: 'Apple EarPods',
+    price: 2327,
+    rate: 4.5,
   },
   {
     id: 3,
     img: earpods2,
-    title: 'Apple BYZ S852I',
-    price: 1000,
-    rate: 4.3,
+    title: 'Apple EarPods',
+    price: 2327,
+    rate: 4.5,
   },
   {
     id: 4,
     img: byzS8521,
     title: 'Apple BYZ S852I',
-    price: 1000,
-    rate: 4.3,
+    price: 2927,
+    rate: 4.7,
+  },
+  {
+    id: 5,
+    img: earpods1,
+    title: 'Apple EarPods',
+    price: 2327,
+    rate: 4.5,
+  },
+  {
+    id: 6,
+    img: earpods2,
+    title: 'Apple EarPods',
+    price: 2327,
+    rate: 4.5,
   },
 ];
