162bcc513155774ff0d622d96ac196f98e259226
ax1lebafer <raul8917@gmail.com>
2025-04-03 19:21:44 +0400

    Cart page

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,11 +1,26 @@
 import { useTranslation } from 'react-i18next';
+import styles from './CartPage.module.scss';
 
 export const CartPage = () => {
   const { t } = useTranslation();
 
   return (
     <>
-      <h3>{t('cart')}</h3>
+      <h3 className={styles.title}>{t('cart')}</h3>
+
+      <div className={styles.cart}>
+        <div className={styles.cartItems}>123</div>
+
+        <div className={styles.cartSummary}>
+          <div className={styles.summaryTop}>
+            <p>{t('total')}</p>
+            <p>₽ 0</p>
+          </div>
+          <button className={styles.proceedBtn}>
+            {t('proceedToCheckout')}
+          </button>
+        </div>
+      </div>
     </>
   );
 };
